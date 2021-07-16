06f5dd8058e5f3d2001e97733ebde905d322bc23
davids89034@gmail.com <davids89034@gmail.com>
2021-07-16 14:02:38 +0800

    final changes

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ import * as fs from 'fs/promises';
 http.createServer(async (req, res)=>{
     const baseURL = "http://localhost:8080";
     const reqURL = new URL(req.url, baseURL);
-    console.log(reqURL.pathname);
     const desPath = reqURL.pathname === "/" ? "index.html" : reqURL.pathname.substring(1) + ".html";
-    console.log(desPath);
     try{
         const data = await fs.readFile(desPath);
         res.writeHead(200, {"Content-Type": "text/html"});
